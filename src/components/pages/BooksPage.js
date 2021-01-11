86e83def9d335a713babe01e7717720b57ce1716
Feyo <nemirov.andrei@gmail.com>
2021-01-11 05:13:41 +0300

    Add Main Page, 404 page, route for books

--- src/components/pages/BooksPage.js
+++ src/components/pages/BooksPage.js
@@ -2,51 +2,39 @@ import React, { Component } from "react";
 import ItemList from "../ItemList";
 import ErrorMessage from "../errorMessage/errorMessage";
 import GotService from "../../services/gotService";
-import RowBlock from "../RowBlock/rowBlock";
-import ItemDetails, { Field } from "../ItemDetails";
+import { withRouter } from "react-router-dom";
 
-export default class BooksPage extends Component {
+class BooksPage extends Component {
   gotService = new GotService();
   state = {
-    selectedBook: 1,
     error: false,
-  };
-  onItemSelected = (id) => {
-    this.setState({
-      selectedBook: id,
-    });
   };
 
   componentDidCatch() {
-    //! хук который обрабатывает ошибки в нашем приложении. Если происходит ошибка то можно отрубить текущий компонент вместо всего приложения
+    //! хук который обрабатывает ошибки в нашем приложении. Если происходит
+    // ошибка то можно отрубить текущий компонент вместо всего приложения
     this.setState({
       error: true,
     });
   }
+
   render() {
     if (this.state.error) {
       return <ErrorMessage />;
     }
 
-    const itemList = (
+    //Возвращаем только список книг, для того, чтобы по клику на них
+    // происходил роутенг на страницу книги
+    return (
       <ItemList
-        onItemSelected={this.onItemSelected}
+        onItemSelected={(itemId) => {
+          this.props.history.push(`/books/${itemId}`);
+        }}
         getData={this.gotService.getAllBooks}
         renderItem={({ name }) => `${name}`}
       />
     );
-
-    const bookDetails = (
-      <ItemDetails
-        itemId={this.state.selectedBook}
-        getData={this.gotService.getBook}
-      >
-        <Field field="publisher" label="Publisher" />
-        <Field field="released" label="Released" />
-        <Field field="numberOfPages" label="Number of pages" />
-      </ItemDetails>
-    );
-
-    return <RowBlock left={itemList} right={bookDetails} />;
   }
 }
+
+export default withRouter(BooksPage);
